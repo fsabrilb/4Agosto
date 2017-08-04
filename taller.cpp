879301629fe9f5b6267fb031b7eb0bebbebec9a3
Felipe Segundo Abril Bermudez <fsabrilb@unal.edu.co>
2017-08-04 11:31:56 -0500

    Es el 5 punto de los numeros primos entre 500 y 1400

---
+++ taller.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int i;
+  int k;
+  int N = 0;
+  for(i=500; i<=1400; i++)
+    {
+      for(N=0, k=1; k<=i; k++)
+	{
+	  if(i%k==0)
+	    N+=1;
+	  else
+	    N+=0;
+	}
+      if(N==2)
+	cout << i <<endl;
+    }
+}
+
